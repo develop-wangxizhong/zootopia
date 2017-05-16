eadc960e1f46d64daf4702fbb913b22fbfa38566
wangxizhong <1025514023@qq.com>
2017-05-16 16:41:52 +0800

    commit modify gradle

---
+++ web/web-usercenter/src/main/java/zootopia/usercenter/UserCenterControllerApplication.java
@@ -0,0 +1,12 @@
+package zootopia.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCenterControllerApplication.class, args);
+	}
+}
